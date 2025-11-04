eacd1e6982abaea282ad9e8585d931235ab84e44
Adriana Gusmão <adriana.gusmao@outlook.com>
2025-11-04 15:47:34 -0300

    Primeiros scripts SQL

---
+++ consultas.sql
@@ -0,0 +1,14 @@
+ALTER TABLE compras RENAME TO vendas; -- Alterando o nome da tabela de compras para vendas
+
+ALTER TABLE vendas RENAME COLUMN customer_id TO id_cliente;  -- Alterando o nome das colunas da tabela vendas
+ALTER TABLE vendas RENAME COLUMN product TO produto;
+ALTER TABLE vendas RENAME COLUMN category TO categoria;
+ALTER TABLE vendas RENAME COLUMN quantity TO quantidade;
+ALTER TABLE vendas RENAME COLUMN discount_percent TO percentual_desconto;
+ALTER TABLE vendas RENAME COLUMN total_value TO valor_total;
+ALTER TABLE vendas RENAME COLUMN purchase_date TO data_compra;
+
+SELECT * FROM vendas; -- Vizualizando a tabela inteira
+
+
+
